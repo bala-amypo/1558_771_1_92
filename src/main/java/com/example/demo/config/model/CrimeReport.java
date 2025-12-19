5d9fda66698bcaac4583e89f43638da4f70ca5c9
bala-amypo <bala@amypo.in>
2025-12-19 04:30:22 +0000

    User commit

--- src/main/java/com/example/demo/config/model/CrimeReport.java
+++ src/main/java/com/example/demo/config/model/CrimeReport.java
@@ -1,3 +1,8 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
 @Entity
 public class CrimeReport {
 
@@ -7,7 +12,6 @@ public class CrimeReport {
 
     private double latitude;
     private double longitude;
-
     private LocalDateTime occurredAt;
 
     public double getLatitude() { return latitude; }
