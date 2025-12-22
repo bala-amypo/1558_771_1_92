40594dd084eebd94634440b606293c4b436b32b1
bala-amypo <bala@amypo.in>
2025-12-22 05:26:01 +0000

    User commit

--- src/main/java/com/example/demo/controller/ AnalysisLogController.java
+++ src/main/java/com/example/demo/controller/ AnalysisLogController.java
@@ -18,10 +18,3 @@ public class AnalysisLogController {
         return "Logs fetched";
     }
 }
-cd /home/coder/Workspace/demo/src/main/java/com/example/demo/controller/
-mv " AnalysisLogController.java" AnalysisLogController.java
-mv " HotspotZoneController.java" HotspotZoneController.java
-
-cd /home/coder/Workspace/demo/src/main/java/com/example/demo/service/
-mv " AnalysisLogService.java" AnalysisLogService.java
-mv " PatternDetectionService.java" PatternDetectionService.java
